6fd4b23f20cd19007ef6ada62bb09201ee6fb3a7
rchaser53 <tayoshizawa29@gmail.com>
2017-12-27 17:04:24 +0900

    run nuxt.js with typescript

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,13 @@ module.exports = {
     vendor: [
       'axios',
       'vuex'
-    ]
-  },
+    ],
+    extend (config) {
+      for (rule of config.module.rules) {
+        if (rule.loader === 'vue-loader') {
+          rule.options.loaders.ts = 'ts-loader?{"appendTsSuffixTo":["\\\\.vue$"]}'
+        }
+      }
+    }
+  }
 }
