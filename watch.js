21f825dff1548d46e5a031282df3454bb16ebfc9
rchaser53 <tayoshizawa29@gmail.com>
2017-12-27 15:20:42 +0900

    add watch

---
+++ watch.js
@@ -0,0 +1,6 @@
+const chokidar = require('chokidar');
+const shell = require('shelljs');
+
+chokidar.watch('./src/ts', {ignored: /(^|[\/\\])\../}).on('change', (event, path) => {
+  shell.exec('npm run tsc')
+});
